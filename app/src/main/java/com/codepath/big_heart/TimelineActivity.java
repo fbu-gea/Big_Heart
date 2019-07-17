8ced367ca3a97518a605e9be3445b23f1053039a
Elana Hummel <ehummel@fb.com>
2019-07-17 11:23:17 -0700

    added layout files for each fragment

--- app/src/main/java/com/codepath/big_heart/TimelineActivity.java
+++ app/src/main/java/com/codepath/big_heart/TimelineActivity.java
@@ -2,18 +2,14 @@ package com.codepath.big_heart;
 
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.codepath.big_heart.fragments.HomeFragment;
-import com.codepath.big_heart.fragments.PostFragment;
+import com.codepath.big_heart.fragments.MapsFragment;
 import com.codepath.big_heart.fragments.ProfileFragment;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class TimelineActivity extends AppCompatActivity {
 
@@ -33,39 +29,39 @@ public class TimelineActivity extends AppCompatActivity {
 
 //        // define your fragments here
         final Fragment homeFragment = new HomeFragment();
-        final Fragment postFragment = new PostFragment();
+        final Fragment postFragment = new MapsFragment();
         final Fragment profileFragment = new ProfileFragment();
 
-        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+//        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+//        getSupportActionBar().setDisplayShowTitleEnabled(false);
+//
+//        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
 
         // handle navigation selection
-        bottomNavigationView.setOnNavigationItemSelectedListener(
-                new BottomNavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                        Fragment fragment;
-                        switch (item.getItemId()) {
-                            case R.id.miHome:
-                                fragment = homeFragment;
-                                break;
-                            case R.id.miAddPost:
-                                fragment = postFragment;
-                                break;
-                            case R.id.miProfile:
-                                fragment = profileFragment;
-                                break;
-                            default:
-                                fragment = homeFragment;
-                                break;
-                        }
-                        fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
-                        return true;
-                    }
-                });
-        // Set default selection
-        bottomNavigationView.setSelectedItemId(R.id.miHome);
+//        bottomNavigationView.setOnNavigationItemSelectedListener(
+//                new BottomNavigationView.OnNavigationItemSelectedListener() {
+//                    @Override
+//                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                        Fragment fragment;
+//                        switch (item.getItemId()) {
+//                            case R.id.miHome:
+//                                fragment = homeFragment;
+//                                break;
+//                            case R.id.miAddPost:
+//                                fragment = postFragment;
+//                                break;
+//                            case R.id.miProfile:
+//                                fragment = profileFragment;
+//                                break;
+//                            default:
+//                                fragment = homeFragment;
+//                                break;
+//                        }
+//                        fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+//                        return true;
+//                    }
+//                });
+//        // Set default selection
+//        bottomNavigationView.setSelectedItemId(R.id.miHome);
     }
 }
